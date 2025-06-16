19ea0f6a8f4cd9bd7813536fe4117554e7055f38
Michael Pollind <mpollind@gmail.com>
2025-06-15 23:15:37 -0700

    feat: add sse handler
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- web/index.ts
+++ web/index.ts
@@ -1,6 +1,7 @@
 import { morph } from '@alpinejs/morph';
 import Alpine, { data } from 'alpinejs';
 import { sse } from './sse';
+import './index.css'
 window["Alpine"] = Alpine
 
 Alpine.plugin(morph);
